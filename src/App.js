f4acfe96242fec6dca8ed144357f7f49cfcf0a41
Matteo Strazzera <99515747+Matstraz@users.noreply.github.com>
2022-09-12 17:45:43 +0200

    feat: add privacy component

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import { Dashboard } from "./components/pages/Dashboard";
 import { ProtectedArea } from "./components/protection/ProtectedArea";
 import { Easter } from "./components/protection/Easter";
 import { Feature } from "./components/pages/Feature";
+import Privacy from "./components/pages/Privacy";
 
 export function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/feature" element={<Feature />} />
+      <Route path="/privacy" element={<Privacy />} />
       <Route element={<ProtectedArea />}>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/dashboard/easteregg" element={<Easter />} />
---
+++ src/components/pages/Privacy.js
@@ -0,0 +1,186 @@
+import { Header } from "../header/Header";
+
+export default function Privacy() {
+  return (
+    <div>
+      <Header />
+      <div className="bg-gradient-to-b from-violet-900 to-black text-white px-32">
+        <h1 className="h1Size">Privacy Policy</h1>
+        <p>
+          Last updated: February 15, 2022 <br /> This Privacy Policy describes
+          Our policies and procedures on the collection, use and disclosure of
+          Your information when You use the Service and tells You about Your
+          privacy rights and how the law protects You. <br /> We use Your
+          Personal data to provide and improve the Service. By using the
+          Service, You agree to the collection and use of information in
+          accordance with this Privacy Policy.
+        </p>
+        <h1 className="h1Size mt-16"> Interpretation and Definitions</h1>
+
+        <h2 className="h2Size mt-5">Interpretation</h2>
+        <p>
+          {" "}
+          The words of which the initial letter is capitalized have meanings
+          defined under the following conditions. The following definitions
+          shall have the same meaning regardless of whether they appear in
+          singular or in plural.
+        </p>
+        <h2 className="h2Size mt-5">Definitions</h2>
+        <br />
+
+        <p>For the purposes of this Privacy Policy:</p>
+
+        <ul className="list-disc ml-8">
+          <li>
+            Account means a unique account created for You to access our Service
+            or parts of our Service.
+          </li>
+          <li>
+            Company (referred to as either "the Company", "We", "Us" or "Our" in
+            this Agreement) refers to Optionsfy LLC, 7901 4th St N, STE 300 St.
+            Petersburg , FL 33702.
+          </li>
+          <li>
+            Cookies are small files that are placed on Your computer, mobile
+            device or any other device by a website, containing the details of
+            Your browsing history on that website among its many uses.
+          </li>
+          <li>Country refers to: Florida, United States</li>
+          <li>
+            Device means any device that can access the Service such as a
+            computer, a cellphone or a digital tablet.
+          </li>
+          <li>
+            Personal Data is any information that relates to an identified or
+            identifiable individual.
+          </li>
+          <li>Service refers to the Website.</li>
+          <li>
+            Service Provider means any natural or legal person who processes the
+            data on behalf of the Company. It refers to third-party companies or
+            individuals employed by the Company to facilitate the Service, to
+            provide the Service on behalf of the Company, to perform services
+            related to the Service or to assist the Company in analyzing how the
+            Service is used.
+          </li>
+          <li>
+            Usage Data refers to data collected automatically, either generated
+            by the use of the Service or from the Service infrastructure itself
+            (for example, the duration of a page visit).
+          </li>
+          <li>
+            Website refers to Optionsfy, accessible from
+            https://www.optionsfy.com/
+          </li>
+          <li>
+            You means the individual accessing or using the Service, or the
+            company, or other legal entity on behalf of which such individual is
+            accessing or using the Service, as applicable.
+          </li>
+        </ul>
+        <h1 className="h1Size mt-8">Collecting and Using Your Personal Data</h1>
+        <h2 className="h2Size mt-6">Types of Data Collected</h2>
+        <h2 className="h2Size mt-6">Personal Data</h2>
+        <p>
+          While using Our Service, We may ask You to provide Us with certain
+          personally identifiable information that can be used to contact or
+          identify You. Personally identifiable information may include, but is
+          not limited to:
+        </p>
+
+        <ul className="list-disc ml-8">
+          <li>Email address</li>
+          <li>First name and last name</li>
+          <li>Phone number</li>
+          <li>Usage Data</li>
+        </ul>
+
+        <h2 className="h2Size mt-6">Usage Data</h2>
+        <p>
+          Usage Data is collected automatically when using the Service. <br />
+          Usage Data may include information such as Your Device's Internet
+          Protocol address (e.g. IP address), browser type, browser version, the
+          pages of our Service that You visit, the time and date of Your visit,
+          the time spent on those pages, unique device identifiers and other
+          diagnostic data. <br />
+          When You access the Service by or through a mobile device, We may
+          collect certain information automatically, including, but not limited
+          to, the type of mobile device You use, Your mobile device unique ID,
+          the IP address of Your mobile device, Your mobile operating system,
+          the type of mobile Internet browser You use, unique device identifiers
+          and other diagnostic data. <br />
+          We may also collect information that Your browser sends whenever You
+          visit our Service or when You access the Service by or through a
+          mobile device.
+        </p>
+
+        <h2 className="h2Size mt-6">Tracking Technologies and Cookies</h2>
+        <p>
+          We use Cookies and similar tracking technologies to track the activity
+          on Our Service and store certain information. Tracking technologies
+          used are beacons, tags, and scripts to collect and track information
+          and to improve and analyze Our Service. The technologies We use may
+          include:
+        </p>
+
+        <ul className="list-disc ml-8">
+          <li>
+            Cookies or Browser Cookies. A cookie is a small file placed on Your
+            Device. You can instruct Your browser to refuse all Cookies or to
+            indicate when a Cookie is being sent. However, if You do not accept
+            Cookies, You may not be able to use some parts of our Service.
+            Unless you have adjusted Your browser setting so that it will refuse
+            Cookies, our Service may use Cookies.
+          </li>
+          <li>First name and last name</li>
+          <li>
+            Flash Cookies. Certain features of our Service may use local stored
+            objects (or Flash Cookies) to collect and store information about
+            Your preferences or Your activity on our Service. Flash Cookies are
+            not managed by the same browser settings as those used for Browser
+            Cookies. For more information on how You can delete Flash Cookies,
+            please read "Where can I change the settings for disabling, or
+            deleting local shared objects?" available at
+            https://helpx.adobe.com/flash-player/kb/disable-local-shared-objects-flash.html#main_Where_can_I_change_the_settings_for_disabling__or_deleting_local_shared_objects_
+          </li>
+          <li>
+            Web Beacons. Certain sections of our Service and our emails may
+            contain small electronic files known as web beacons (also referred
+            to as clear gifs, pixel tags, and single-pixel gifs) that permit the
+            Company, for example, to count users who have visited those pages or
+            opened an email and for other related website statistics (for
+            example, recording the popularity of a certain section and verifying
+            system and server integrity).
+          </li>
+        </ul>
+        <br />
+        <p>
+          Cookies can be "Persistent" or "Session" Cookies. Persistent Cookies
+          remain on Your personal computer or mobile device when You go offline,
+          while Session Cookies are deleted as soon as You close Your web
+          browser. You can learn more about cookies here: Cookies by TermsFeed
+          Generator. <br />
+          We use both Session and Persistent Cookies for the purposes set out
+          below:
+        </p>
+
+        <ul className="list-disc ml-8">
+          <li className="font-bold">Necessary / Essential Cookies</li>
+          <p>Type: Session Cookies</p>
+          <p>Administered by: Us</p>
+          <p>
+            Purpose: These Cookies are essential to provide You with services
+            available through the Website and to enable You to use some of its
+            features. They help to authenticate users and prevent fraudulent use
+            of user accounts. Without these Cookies, the services that You have
+            asked for cannot be provided, and We only use these Cookies to
+            provide You with those services.
+          </p>
+          <li>First name and last name</li>
+          <li>Phone number</li>
+          <li>Usage Data</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
