b428236755011bbfa88f80240d15c42e8433d7fa
Aida Brugnone <99371449+AidaB9837@users.noreply.github.com>
2022-09-12 16:39:00 +0200

    feat: transform from class to function components

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { MainNavbar } from "./MainNavbar";
 
-export class Header extends React.Component {
-  render() {
-    return (
-      <div className="border-b-2 border-black gradient-nav">
-        <MainNavbar />
-      </div>
-    );
-  }
+export function Header() {
+  return (
+    <div className="border-b-2 border-black gradient-nav">
+      <MainNavbar />
+    </div>
+  );
 }
